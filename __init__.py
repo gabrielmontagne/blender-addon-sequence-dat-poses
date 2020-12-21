2df41ca249e7220ab9969bbde57479fe68a8d3a9
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-12-22 00:17:25 +0100

    initial import

---
+++ __init__.py
@@ -0,0 +1,14 @@
+bl_info = {
+    'name': 'Sequence .dat poses',
+    'author': 'gabriel montagné, gabriel@tibas.london',
+    'version': (0, 0, 1),
+    'blender': (2, 80, 0),
+    'description': "Applies .dat pose library names to selected bones",
+    'category': 'Animation'
+}
+
+def register():
+    print('Registering Seqnce DAT poses')
+
+def unregister():
+    print('Registering Seqnce DAT poses')
