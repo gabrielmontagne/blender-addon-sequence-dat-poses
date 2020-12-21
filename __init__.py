67347db39c314ac8cd00d3452754a9c3168035af
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-12-22 00:52:27 +0100

    load .dat file

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+from bpy.props import StringProperty, BoolProperty
+from bpy.types import Operator
+from bpy_extras.io_utils import ExportHelper, ImportHelper
+from bpy.utils import register_class, unregister_class
+
 bl_info = {
     'name': 'Sequence .dat poses',
     'author': 'gabriel montagné, gabriel@tibas.london',
@@ -7,8 +12,27 @@ bl_info = {
     'category': 'Animation'
 }
 
+class ANIMATION_OT_sequence_dat_poses(Operator, ImportHelper):
+    """Sequence poses from .dat animation file"""
+    bl_idname = "animation.dat_sequence_animation"
+    bl_label = "Sequence poess from .dat animation file"
+
+    filename_ext = ".dat"
+    filter_glob: StringProperty(default="*.dat", options={'HIDDEN'}, maxlen=255)
+
+    def execute(self, context):
+        scene = context.scene
+
+        with open(self.properties.filepath, 'r') as f:
+            for line in f:
+                print('line', line.strip())
+
+        return {'FINISHED'}
+
 def register():
     print('Registering Seqnce DAT poses')
+    register_class(ANIMATION_OT_sequence_dat_poses)
 
 def unregister():
     print('Registering Seqnce DAT poses')
+    unregister_class(ANIMATION_OT_sequence_dat_poses)
