8e758d27b03ddf7ac1c4dd7a69b0ab59cbd84721
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-12-22 05:43:44 +0100

    Bump version: 0.0.1 → 0.0.2

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ from re import compile
 bl_info = {
     'name': 'Sequence .dat poses',
     'author': 'gabriel montagné, gabriel@tibas.london',
-    'version': (0, 0, 1),
+    'version': (0, 0, 2),
     'blender': (2, 80, 0),
     'description': "Applies .dat pose library names to selected bones",
     'category': 'Animation'
