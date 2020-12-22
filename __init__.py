a0a26589fc980b5cd48ecf7ad5e919b671e5582c
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-12-22 02:23:02 +0100

    apply (but not yet keyining) the poses

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
+import bpy
 from bpy.props import StringProperty, BoolProperty
 from bpy.types import Operator
-from bpy_extras.io_utils import ExportHelper, ImportHelper
 from bpy.utils import register_class, unregister_class
+from bpy_extras.io_utils import ExportHelper, ImportHelper
 from re import compile
 
 bl_info = {
@@ -26,17 +27,32 @@ class ANIMATION_OT_sequence_dat_poses(Operator, ImportHelper):
     @classmethod
     def poll(cls, context):
         a = context.active_object
-        return a and a.type == 'ARMATURE'
+        return a and a.type == 'ARMATURE' and a.pose_library
 
     def execute(self, context):
         scene = context.scene
+        lib = context.active_object.pose_library
+        markers = lib.pose_markers
 
         with open(self.properties.filepath, 'r') as f:
+            not_found = set()
             for line in f:
                 m = dat_line.fullmatch(line.strip())
                 if not m: continue
-                i, t = m.groups()
-                print('yvar', i, t)
+                f, t = m.groups()
+                i = markers.find(t)
+                if i == -1:
+                    not_found.add(t)
+                    continue
+
+                p = markers.get(t)
+                print('yvar', i, f, t, p)
+
+                context.scene.frame_current = int(f)
+                bpy.ops.poselib.apply_pose(pose_index=i)
+
+            if not_found:
+                self.report({'WARNING'}, "Couldn't find {} poses".format(not_found))
 
         return {'FINISHED'}
 
